d6bcd8cae91df13f418bbe12192ff7f920c5972b
Danlc2711 <166543121+Danlc2711@users.noreply.github.com>
2025-09-23 07:15:27 +0700

    Delete PetProject/PetProject directory

--- PetProject/PetProject/start-tomcat.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Tomcat...
-set CATALINA_HOME=C:\Users\ADMIN\Desktop\apache-tomcat-10.1.46-windows-x64\apache-tomcat-10.1.46
-call "%CATALINA_HOME%\bin\startup.bat"
-echo Tomcat started!
-pause
--- PetProject/PetProject/stop-tomcat.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Stopping Tomcat...
-set CATALINA_HOME=C:\Users\ADMIN\Desktop\apache-tomcat-10.1.46-windows-x64\apache-tomcat-10.1.46
-call "%CATALINA_HOME%\bin\shutdown.bat"
-echo Tomcat stopped!
-pause
